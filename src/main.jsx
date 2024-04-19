2e3f72bdf5e460969e6f28ebd5d6aaa1505770e8
Pandu Sabdanabi <sabdanabi>
2024-04-19 21:33:20 +0700

    feat:[PS] add login UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import RegisterPage from "./pages/auth_pages/RegisterPage.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RegisterPage/>
   </React.StrictMode>,
 )
