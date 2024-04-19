00b5495a781b7388f9754bccf22cbfcf4bd8409f
Pandu Sabdanabi <sabdanabi>
2024-04-19 21:34:38 +0700

    feat: [PS] add login and register UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import RegisterPage from "./pages/auth_pages/RegisterPage.jsx";
+import LoginPage from "./pages/auth_pages/LoginPage.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RegisterPage/>
+    <LoginPage/>
   </React.StrictMode>,
 )
