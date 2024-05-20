f155ada12d2c9fb2736dc24397d351aef5d24248
Pandu Sabdanabi <sabdanabi>
2024-05-20 20:58:28 +0700

    feat: [PS] menambahkan fitur popup di page persediaan

--- src/pages/main_page/PersediaanPage.jsx
+++ src/pages/main_page/PersediaanPage.jsx
@@ -2,17 +2,14 @@ import SideNavbarComponent from "../../components/components_reused/SideNavbarCo
 import PartTop from "../../components/components_reused/PartTop.jsx";
 import TblStock from "../../components/page_persediaan_components/TblStock.jsx";
 import BtnAddStock from "../../components/page_persediaan_components/BtnAddStock.jsx";
-import {useState} from "react";
-import PopupTambahProduk from "../../components/popup-components/PopupTambahProduk.jsx";
 
 export default function PersediaanPage() {
-    const [open, setOpen] = useState(false)
     return (
         <div className="flex h-screen overflow-hidden bg-gray-100">
             <SideNavbarComponent/>
             <div className="flex flex-col flex-1 w-full overflow-hidden">
                 <PartTop/>
-                <BtnAddStock setOpen={setOpen} titlePage={"Produk"} titleBtn={"Product"}/>
+                <BtnAddStock titlePage={"Produk"} titleBtn={"Product"}/>
                 <TblStock/>
             </div>
         </div>
